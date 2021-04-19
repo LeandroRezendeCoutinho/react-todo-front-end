ac69c56323448d9709f8b39a1b7bc09e89a93400
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-04-19 08:59:25 -0300

    make buttons work

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -10,11 +10,15 @@ const URL = "http://localhost:3003/api/todos"
 export default class Todo extends Component {
   constructor(props) {
     super(props);
-    this.state = { description: '', list: [] };
+    this.state = { description: '', list: [] }
 
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+
     this.refresh()
   }
 
@@ -38,14 +42,28 @@ export default class Todo extends Component {
       .then(_response => this.refresh())
   }
 
+  handleMarkAsDone(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+      .then(_response => this.refresh())
+  }
+
+  handleMarkAsPending(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+      .then(_response => this.refresh())
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-        <TodoForm description={this.state.description}
+        <TodoForm
+          description={this.state.description}
           handleChange={this.handleChange}
           handleAdd={this.handleAdd} />
-        <TodoList list={this.state.list}
+        <TodoList
+          list={this.state.list}
+          handleMarkAsDone={this.handleMarkAsDone}
+          handleMarkAsPending={this.handleMarkAsPending}
           handleRemove={this.handleRemove} />
       </div>
     )
